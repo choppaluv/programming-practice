605fc57831b496113c0e0c420d1d362b5c8f140e
Jay B. Kim <kim.jayb@gmail.com>
2016-10-13 18:54:00 +0900

    Review on rank.c
    In the first 'for' loop, { and } are missing.

--- 1012/rank.c
+++ 1012/rank.c
@@ -2,11 +2,13 @@
 int main()
 {
     int i=0, j=0, n;
-    int score[1001], rank[1001];
+    int score[1000], rank[1000];  // why 1001?  this array is not for string!
     scanf("%d",&n);
-    for (i=0;i<n; i++)
+    for (i=0;i<n; i++) // C isn't like Python, be careful not to omit { }
+    {
 	scanf("%d",&score[i]);
 	rank[i]=0;
+    }
     for (i=0;i<n;i++)
     {
 	for (j=0;j<n;j++)
